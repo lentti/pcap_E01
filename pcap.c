4c4013ff7046af1908e77542a599a505b133b129
lentti <shinmg2520@gmail.com>
2017-07-15 12:30:54 +0900

    Add printPacket module

--- pcap.c
+++ pcap.c
@@ -1,5 +1,14 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <ctype.h>
+
+#define ETHERNET_HEAD_SIZE (16)
+#define MAC_ADDR_LEN (6)
+#define ETHER_TYPE (4)
+#define IP_TYPE (0x0800)
+
+void printPacket(u_char*,int);
+
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +21,7 @@ int main(int argc, char *argv[])
     bpf_u_int32 net;		/* Our IP */
     struct pcap_pkthdr *header;	/* The header that pcap gives us */
     const u_char *packet;		/* The actual packet */
-    int retValue;
+    int retValue=0,i;
 
     /* Define the device */
     dev = pcap_lookupdev(errbuf);
@@ -44,11 +53,51 @@ int main(int argc, char *argv[])
     while (1){
         /* Grab a packet */
         retValue = pcap_next_ex(handle, &header, &packet);
+        /* Check empty packet */
+        if ( header->len ==0 )
+            continue;
         /* Print its length */
         printf("Jacked a packet with length of [%d]\n", header->len);
-        printf("%c",*packet);
+        printPacket(packet,header->len);
+        printf("\n");
         /* And close the session */
     }
     pcap_close(handle);
     return(0);
+}
+
+void printPacket(u_char* packet,int len)
+{
+    int i;
+    for ( i=0; i < len ; i++ ){
+        if (i%16 ==0 && i != 0){
+            printf("  ");
+            for ( int j=-16;j<=-1;j++ ){
+                if (j == -8)
+                    printf("  ");
+                if (isprint(*(packet+i+j)))
+                    printf("%c", *(packet+i+j));
+                else
+                    printf(".");
+            }
+            printf("\n");
+        }
+        if ( i % 8 ==0 )
+            printf ("  ");
+        printf("%02x ", *(packet+i));
+    }
+    for(i=0;i<16-(len%16);i++){
+        printf("   ");
+        if ( i % 8 ==0 )
+            printf ("  ");
+    }
+    for ( int i=(len/16)*16;i<len;i++ ){
+        if (i%8 == 0 && i%16 != 0)
+            printf("  ");
+        if (isprint(*(packet+i)))
+            printf("%c", *(packet+i));
+        else
+            printf(".");
+    }
+    printf("\n\n");
 }
